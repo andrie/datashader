71dd1cea6d7532930d06ae508fc20bee985aa722
Andrie de Vries <apdevries@gmail.com>
2019-04-23 08:58:32 +0100

    Simplify code using scale_01()

--- src/discretize.cpp
+++ src/discretize.cpp
@@ -1,6 +1,11 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+float scale_01(float x, float min_x, float max_x){
+  return ((x - min_x) / (max_x - min_x));
+}
+
+
 // [[Rcpp::export]]
 NumericMatrix discretize_cpp(DataFrame data, NumericVector dims,
                              NumericVector x_range, NumericVector y_range) {
@@ -15,12 +20,12 @@ NumericMatrix discretize_cpp(DataFrame data, NumericVector dims,
   NumericVector x = data[0];
   NumericVector y = data[1];
 
-  unsigned long int xsize = data.nrow();
-  for (unsigned long int i = 0; i < xsize; i++){
-    int xx = round(((x[i] - x_range[0]) / (x_range[1] - x_range[0])) * (rows - 1));
-    int yy = round(((y[i] - y_range[0]) / (y_range[1] - y_range[0])) * (cols - 1));
+  unsigned long xsize = data.nrow();
+  for (unsigned long i = 0; i < xsize; i++){
+    int xx = round(scale_01(x[i], x_range[0], x_range[1]) * (rows - 1));
+    int yy = round(scale_01(y[i], y_range[0], y_range[1]) * (cols - 1));
     // Rprintf("xx: %2i, yy: %2i\n", xx, yy);
-    z(xx, yy) += 1;
+    if (xx <= rows && yy <= cols) z(xx, yy) += 1;
   }
   return z;
 }
